58219d412edaf4ea928447deb3d67f23f6c42e76
subeshb1 <subeshb1@gmail.com>
2018-07-18 01:03:42 +0545

    Depoloy

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
 import { sorting } from "./reducers";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 
 const configureStore = () => {
   const store = createStore(
     sorting,
-    composeWithDevTools(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   );
 
   return store;
