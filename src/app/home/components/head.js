09039405bc2a335c495272edd875477a1cf340a4
subeshb1 <subeshb1@gmail.com>
2018-09-11 20:23:39 +0545

    Site Map

--- src/app/home/components/head.js
+++ src/app/home/components/head.js
@@ -6,10 +6,14 @@ export default class Head extends Component {
     return (
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Simple Algorithms</title>
+        <title>Simple Algorithms - Learn, Visualize, Implement</title>
         <meta
           name="description"
-          content="Simple Algorithms, a site to visualize, implement, learn and implement algorithms"
+          content="Simple Algorithms, a site to visualize, implement, learn and implement algorithms. Learn the basics to Design and Analysis of Algorithms. Compute Complexity, Easy Visualization, Algorithm Codes all in one place."
+        />
+        <meta
+          name="keyword"
+          content="algorithm,visualize,implementation,bfs,algorithm complexity,big o,time complexity,space complexity,"
         />
       </Helmet>
     );
