4ed9ae75242cbf1d26e51f7e1ea2df6891030aaf
subeshb1 <subeshb1@gmail.com>
2018-09-13 07:46:36 +0545

    sitemap

--- src/app/games/components/snake-tut.js
+++ src/app/games/components/snake-tut.js
@@ -8,6 +8,10 @@ export default class SnakeTut extends Component {
           <h1>Snake Game</h1>
           <article>
             <h2>Introduction</h2>
+            <pre>{`
+            
+         
+            `}</pre>
             <p>
               Hello Guys, Today I'll be walking you through step by step process
               to build a simple snake game.
