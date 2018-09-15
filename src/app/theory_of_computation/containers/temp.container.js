043c444688fc47c144dbe073a5cedd5a6522c1a3
subeshb1 <subeshb1@gmail.com>
2018-09-15 22:12:22 +0545

    Starting TOC

---
+++ src/app/theory_of_computation/containers/temp.container.js
@@ -0,0 +1,30 @@
+import { connect } from "react-redux";
+import {} from "../components";
+import {} from "../actions";
+import React, { Component } from 'react'
+
+class Container extends Component {
+  render() {
+      const {} = this.props;
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+const mapStateToProps = (state, ownProps) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)();
---
+++ src/app/theory_of_computation/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './css/index.css';
+import {} from './containers';
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
